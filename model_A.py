c207192ab732cc04dbf05ecadda3bce25b98ea24
Noam Goldberg <noamgoldberg2@gmail.com>
2022-07-05 10:45:23 +0300

    Implemented -verbose as terminal argument for evaluating model_A output

--- model_A.py
+++ model_A.py
@@ -21,8 +21,8 @@ def generate_params(args, num_playlists=10, verbose=0):
     playlists_results = sp.search(q=input_text, limit=num_playlists, type='playlist')['playlists']
     # print("playlist_results:", playlists_results)
     playlist_uris = [playlist['id'] for playlist in playlists_results['items']]
-    if verbose > 0:
-        print("Playlist URIs (list of strings):", playlist_uris)
+    if verbose > 1:
+        print("Playlist URIs (list of strings):", playlist_uris, '\n')
 
     # (2) Get all track uris from playlists in search results
     track_results = [sp.playlist_items(p_uri, limit=100) for p_uri in playlist_uris]
@@ -30,43 +30,41 @@ def generate_params(args, num_playlists=10, verbose=0):
         raise_spotify_error()
     track_uris_dict = {playlist_uris[i]: [track['track']['id'] if track['track'] is not None else None for track in track_results[i]['items']] for i in range(len(playlist_uris))}
     track_uris_dict = {key: [track_uri for track_uri in track_uris_dict[key] if track_uri] for key in track_uris_dict}
-    if verbose > 0:
+    if verbose > 4:
         print("Track URIs (dict):\n", track_uris_dict)
 
     # (3) Get audio features of each track in each playlist
     audio_features = [sp.audio_features(tracks=track_uris_dict[playlist]) for playlist in track_uris_dict]
     audio_features = [[track_features for track_features in playlist if track_features] for playlist in audio_features]
-    print('audio_features:', audio_features)
+    if verbose > 4:
+        print("Audio Features (list of ):\n", audio_features)
     audio_features = [[{f: track[f] for f in AUDIO_FEATURES_TO_EXTRACT} for track in playlist] for playlist in
                       audio_features]
     avg_audio_features = dict(pd.concat([pd.DataFrame(audio_features[i]).mean() for i in range(len(playlist_uris))],
                                         axis=1).mean(axis=1))
     avg_audio_features['popularity'] = args.popularity
-    if verbose > 0:
-        print("Features averaged (series):\n", avg_audio_features)
+    if verbose > 1:
+        print("Features averaged (series):\n", avg_audio_features, '\n')
     return avg_audio_features
 
 
-def main(verbose=0):
+def main():
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     args = parse_args(sys.argv[1:])
-    if verbose > 0:
-        print("Parsed args:", args)
+    verbose = args.verbose
+    if verbose > 1:
+        print("\nParsed args:", args, '\n')
     if args.command == 'input':
         try:
             sp = authorize()
             genre_text = predict_genre(args)
-            if verbose > 0:
-                print("Genres:", genre_text)
-            params = generate_params(args, num_playlists=2, verbose=verbose)
-            if verbose > 0:
-                print("Spotify params:", params)
+            if verbose > 1:
+                print("\nGenres:", genre_text, '\n')
+            params = generate_params(args, num_playlists=7, verbose=verbose)
             tracks = recommend(params, genre_text, sp, args)
-            if verbose > 0:
+            if verbose > 1:
                 print("Playlist (Song URIs):", tracks)
             playlist_url = create_spotify_playlist(tracks, args.text, sp, args)
-            if verbose > 0:
-                print("Playlist URL:", playlist_url)
         except ValueError as e:
             print('ValueError:', e)
             logging.critical(e)
@@ -82,4 +80,4 @@ def main(verbose=0):
 
 
 if __name__ == '__main__':
-    main(verbose=0)
+    main()
